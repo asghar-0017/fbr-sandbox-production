95be2f62bce53281d14eefe7d89989cc13336c6a
Asghar Ali <asghar.usman0017@gmail.com>
2025-08-13 04:38:03 +0500

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -42,7 +42,7 @@ app.use(cors({
   origin: ['http://localhost:5174', 'http://localhost:5150','http://localhost:3003','https://united-tubes.inplsoftwares.online','*'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Tenant-ID'],
-}));
+}))
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/invoices', express.static(path.join(process.cwd(), 'public/invoices')))

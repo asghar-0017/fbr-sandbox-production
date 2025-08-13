555ff6a61d80e3efbf96bfcc1a9fd47c9b301869
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-14 04:21:03 +0500

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -42,7 +42,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://united-tubes.inplsoftwares.online",
+      "https://pak-progressive.inplsoftwares.online",
       "http://localhost:3000",
       "https://fbrtestcase.inplsoftwares.online",
       "*",

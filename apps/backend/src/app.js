abe9cda1794e056381e7508a0b500aec59a54041
Asghar Ali <asghar.usman0017@gmail.com>
2025-08-16 12:14:51 -0700

    commenting

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -42,7 +42,7 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        connectSrc: ["'self'", "https://gw.fbr.gov.pk", "https://adnan-power.inplsoftwares.online"],
+        connectSrc: ["'self'", "https://gw.fbr.gov.pk", "https://united-tubes.inplsoftwares.online"],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'"],
         imgSrc: ["'self'", "data:", "https:"],
@@ -55,8 +55,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://adnan-power.inplsoftwares.online",
-      "https://adnan-power.inplsoftwares.online",
+      "https://united-tubes.inplsoftwares.online",
+      "https://united-tubes.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],

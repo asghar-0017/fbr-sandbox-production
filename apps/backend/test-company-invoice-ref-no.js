ad253ac78475e2df3e99fd0593a21ecd9c19c1e4
Asghar Ali <asghar.usman0017@gmail.com>
2025-08-16 13:23:46 -0700

    .

--- apps/backend/test-company-invoice-ref-no.js
+++ apps/backend/test-company-invoice-ref-no.js
@@ -9,7 +9,7 @@
 
 import axios from "axios";
 
-const BASE_URL = "https://paramount-corp.inplsoftwares.online";
+const BASE_URL = "https://machinery-zone.inplsoftwares.online";
 
 async function testCompanyInvoiceRefNo() {
   try {

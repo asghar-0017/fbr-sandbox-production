7cf32d282a92cf6b63b2409ce810df1e18152cff
Asghar Ali <asghar.usman0017@gmail.com>
2025-08-17 23:24:09 -0700

    build upload

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://aja-enterprises.inplsoftwares.online",
+        target: "https://pakistan-gum.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
